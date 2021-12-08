bd85665d487b4ee0e9883d7067be33fea924464b
Andreas0xffff <Andreas0xffff@users.noreply.github.com>
2021-12-08 12:14:24 +0100

    Added nupkg copy to central folder.

--- Build.bat
+++ Build.bat
@@ -1,5 +1,5 @@
-"C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
-cd /d E:\Repos\Andreas0xffff\XamarinCommunityToolkit
+REM "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\Common7\Tools\VsDevCmd.bat"
+REM cd /d E:\Repos\Andreas0xffff\XamarinCommunityToolkit
 
 msbuild.exe samples/XCT.Sample.sln /t:Clean
 
@@ -9,5 +9,8 @@ msbuild.exe src/CommunityToolkit/Xamarin.CommunityToolkit/Xamarin.CommunityToolk
 msbuild.exe src/Markup/Xamarin.CommunityToolkit.Markup/Xamarin.CommunityToolkit.Markup.csproj /p:Configuration=Release /restore /t:Build /p:ContinuousIntegrationBuild=true /p:Deterministic=false
 msbuild.exe src/Markup/Xamarin.CommunityToolkit.Markup/Xamarin.CommunityToolkit.Markup.csproj /t:Pack /p:PackageVersion=9.0.0 /p:PackageOutputPath=nuget
 
-dotnet test src/Markup/Xamarin.CommunityToolkit.Markup.UnitTests/Xamarin.CommunityToolkit.Markup.UnitTests.csproj -c Release --collect "Code coverage" -p:BuildInParallel=false
-dotnet test src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Xamarin.CommunityToolkit.UnitTests.csproj -c Release --collect "Code coverage" -p:BuildInParallel=false
+REM dotnet test src/Markup/Xamarin.CommunityToolkit.Markup.UnitTests/Xamarin.CommunityToolkit.Markup.UnitTests.csproj -c Release --collect "Code coverage" -p:BuildInParallel=false
+REM dotnet test src/CommunityToolkit/Xamarin.CommunityToolkit.UnitTests/Xamarin.CommunityToolkit.UnitTests.csproj -c Release --collect "Code coverage" -p:BuildInParallel=false
+
+copy .\src\CommunityToolkit\Xamarin.CommunityToolkit\nuget\Xamarin.CommunityToolkit.*.nupkg .\nuget\
+copy .\src\Markup\Xamarin.CommunityToolkit.Markup\nuget\Xamarin.CommunityToolkit.Markup.*.nupkg .\nuget\
